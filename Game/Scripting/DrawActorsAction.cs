e5e884919c7b4036782bd8d80cbb9355b9eaf1f9
Michael Hendrick <mhendrick@byui.edu>
2022-11-17 11:13:34 -0700

    Typing hotfix

--- Game/Scripting/DrawActorsAction.cs
+++ Game/Scripting/DrawActorsAction.cs
@@ -32,13 +32,13 @@ namespace Unit05.Game.Scripting
                 segments.AddRange(cycle.GetSegments());
             }
             
-            Actor scores = cast.GetActors("score");
+            List<Actor> scores = cast.GetActors("score");
             Actor food = cast.GetFirstActor("food");
             List<Actor> messages = cast.GetActors("messages");
             
             _videoService.ClearBuffer();
             _videoService.DrawActors(segments);
-            _videoService.DrawActors(score);
+            _videoService.DrawActors(scores);
             _videoService.DrawActor(food);
             _videoService.DrawActors(messages);
             _videoService.FlushBuffer();
